b92f33e6d0fcaa5b9b7b3a902609f0138447009b
Jesse A. Tov <jesse.tov@gmail.com>
2019-10-07 12:27:18 -0500

    script for downloading big files

---
+++ experiments/download.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cd "$(dirname "$0")/files"
+
+grab () {
+    if [ $# = 2 ]; then
+        dst=$1; shift
+    else
+        dst=$(basename "$1")
+    fi
+
+    url=$1
+
+    wget -O"$dst" "$url"
+}
+
+master_url=https://github.com/AndrewBridgeman/Custom-Movie-Maker/blob/master
+
+grab http://www.engr.colostate.edu/me/facil/dynamics/files/drop.avi
+
+for i in 1 2 3 A B C; do
+    grab "$i.mp4" "$master_url/Testing%20API's/$i.mp4?raw=true"
+done
