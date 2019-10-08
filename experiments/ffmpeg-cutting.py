056fb2f29c6dba13d494ba457c4a9a90a258eedb
Andrew Bridgeman <andrewbridgeman@dhcp-10-105-233-2.wireless.northwestern.private>
2019-10-08 15:25:58 -0500

    cutting based on a timestamp

--- experiments/ffmpeg-cutting.py
+++ experiments/ffmpeg-cutting.py
@@ -3,6 +3,14 @@ test2 = 'files/test2.mp4'
 
 from simple_ffmpeg_cutting import Ffmpeg_cutting
 
+timestamp = '00:00:15'
+
 movie = Ffmpeg_cutting('out.mp4')
 movie.append(test1)
+movie.add_times('00:00:00', timestamp)
+movie.write()
+
+movie = Ffmpeg_cutting('out2.mp4')
+movie.append(test1)
+movie.add_times(timestamp, '0')
 movie.write()
--- experiments/simple_ffmpeg_cutting.py
+++ experiments/simple_ffmpeg_cutting.py
@@ -7,12 +7,18 @@ class Ffmpeg_cutting:
     def __init__(self, output):
         self._output = output
         self._inputs = []
+        self._time1 = ''
+        self._time2 = ''
 
     def append(self, input):
         self._inputs.append(input)
 
     def extend(self, inputs):
         self._inputs.extend(inputs)
+
+    def add_times(self, time1, time2):
+        self._time1 = time1
+        self._time2 = time2
 
     def write(self):
         command = self._build_command()
@@ -24,17 +30,14 @@ class Ffmpeg_cutting:
             sys.stderr.write('\n')
             sys.exit(2)
 
-    def _concat(self):
-        count = len(self._inputs) - 1
-        return ','.join(count * ['concat'])
-
     def _build_command(self):
         command = ['ffmpeg', '-y']
         command.append('-ss')
-        command.append('00:00:01')
+        command.append(self._time1)
         command.extend(['-i', self._inputs[0]])
-        command.append('-to')
-        command.append('00:00:10')
+        if self._time2 != '0':
+            command.append('-to')
+            command.append(self._time2)
         command.append('-c')
         command.append('copy')
         command.append(self._output)
