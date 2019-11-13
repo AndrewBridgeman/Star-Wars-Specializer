116a45bf4750ae53ae190b726ca075150bec6de1
Andrew Bridgeman <andrewbridgeman@dhcp-10-105-184-244.wireless.northwestern.private>
2019-11-12 20:05:58 -0600

    file browser (not working)

---
+++ experiments/browser.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from os.path import sep, expanduser, isdir, dirname
+from kivy.garden.filebrowser import FileBrowser
+from kivy.utils import platform
+
+
+class TestApp(App):
+
+    def build(self):
+        if platform == 'win':
+            user_path = dirname(expanduser('~')) + sep + 'Documents'
+        else:
+            user_path = expanduser('~') + sep + 'Documents'
+        browser = FileBrowser(select_string='Select',
+                              favorites=[(user_path, 'Documents')])
+        browser.bind(
+                    on_success=self._fbrowser_success,
+                    on_canceled=self._fbrowser_canceled)
+        return browser
+
+    def _fbrowser_canceled(self, instance):
+        print ('cancelled, Close self.')
+
+    def _fbrowser_success(self, instance):
+        print (instance.selection)
+
+
+TestApp().run()
