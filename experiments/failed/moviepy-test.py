b7ddb3d393320944b28eb81a04ecd2143334e249
Andrew Bridgeman <andrewbridgeman@Andrews-MacBook-Pro-2.local>
2019-12-02 14:13:40 -0600

    cleaning up

--- experiments/failed/moviepy-test.py
+++
@@ -1,7 +0,0 @@
-from moviepy.editor import VideoFileClip, concatenate_videoclips
-clip1 = VideoFileClip("clip1.mp4")
-clip2 = VideoFileClip("clip2.mp4")
-combined_clip = concatenate_videoclips([clip2,clip1], method = 'compose')
-combined_clip=combined_clip.without_audio().set_audio(combined_clip.audio)
-combined_clip.resize(width = 10, height = 500)
-combined_clip.preview(fps = 1, audio = False)
--- experiments/single_scene.py
+++
@@ -1,7 +0,0 @@
-from scene_class import Scene
-
-
-class SingleScene(Scene):
-
-    def __init__(self, start_time, end_time, scene_type):
-        super().__init__(start_time, end_time, scene_type)
